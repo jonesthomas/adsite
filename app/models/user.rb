2f68e8d420aa3643ac8a8e03cba3df6a17cdb586
Justin Jones <justinjones1500@yahoo.com>
2014-01-17 15:01:01 -0500

    Fix password reset unique issue for nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
-  validates :password_reset_token, uniqueness: { case_sensitive: true }
+ validates :password_reset_token, uniqueness: { case_sensitive: true }, allow_nil:true
   has_secure_password
 	validates :password, length: {minimum: 6}
 
